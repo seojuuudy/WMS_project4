5b1313f9051de0b1045e3c426c1fe5c680dc3f79
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-01-31 17:00:10 +0900

    다영 거래처등록 조회

--- WMS_Project/src/main/java/com/itwillbs/wms4/mapper/ClientMapper.java
+++ WMS_Project/src/main/java/com/itwillbs/wms4/mapper/ClientMapper.java
@@ -1,5 +1,31 @@
 package com.itwillbs.wms4.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.itwillbs.wms4.vo.ClientVO;
+
 public interface ClientMapper {
 
+	public int insertClint(ClientVO client);
+
+	public List<ClientVO> selectClientList(
+			@Param("searchType") String searchType, 
+			@Param("keyword") String keyword, 
+			@Param("startRow") int startRow, 
+			@Param("listLimit") int listLimit);
+
+	public int selectClientListCount(
+			@Param("searchType") String searchType, 
+			@Param("keyword") String keyword);
+
+	public ClientVO selectClient(
+			@Param("business_no") String business_no, 
+			@Param("cust_name") String cust_name);
+
+	public String selectBusinessNo(
+			@Param("business_no")String business_no, 
+			@Param("cust_name")String cust_name);
+	
 }
--- WMS_Project/src/main/java/com/itwillbs/wms4/service/ClientService.java
+++ WMS_Project/src/main/java/com/itwillbs/wms4/service/ClientService.java
@@ -1,13 +1,37 @@
 package com.itwillbs.wms4.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwillbs.wms4.mapper.ClientMapper;
+import com.itwillbs.wms4.vo.ClientVO;
 
 @Service
 public class ClientService {
 
 	@Autowired
 	private ClientMapper mapper;
+
+	public int registClient(ClientVO client) {
+		return mapper.insertClint(client);
+	}
+
+	public List<ClientVO> getClientList(String searchType, String keyword, int startRow, int listLimit) {
+		return mapper.selectClientList(searchType, keyword, startRow, listLimit);
+	}
+
+	public int getBoardListCount(String searchType, String keyword) {
+		return mapper.selectClientListCount(searchType, keyword);
+	}
+
+	public ClientVO getClient(String business_no, String cust_name) {
+		return mapper.selectClient(business_no, cust_name);
+	}
+
+	public String CheckBusinessNo(String business_no, String cust_name) {
+		
+		return mapper.selectBusinessNo(business_no, cust_name);
+	}
 }
